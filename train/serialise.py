384b943aad6d54d563809b726ab84b634862cf16
athalang <athalang@hotmail.com>
2024-07-14 02:58:09 +0100

    Add more forwards dataclasses

--- train/serialise.py
+++ train/serialise.py
@@ -14,37 +14,57 @@ class Matrix:
     cols_i: c_uint16
     scale_f: c_int8 # fxp-u8/7 (twos complement)
 
-@dataclass
+@dataclass(kw_only=True)
 class CSRMatrix(Matrix):
-    type_i = 0
-    nnz_i: c_uint16 # number of non-zero elements, length of row_ptrs_a, col_index_a, values_a
-    row_ptrs_a: list[c_uint16]
-    col_index_a: list[c_uint16]
-    values_a: list[c_int8]
+    type_i: c_uint8 = 0
+    nnz_i: c_uint16 # number of non-zero elements
+    row_ptrs_a: list[c_uint16] # length is nnz_i
+    col_index_a: list[c_uint16] # length is nnz_i
+    values_a: list[c_int8] # length is nnz_i
 
 @dataclass
-class Layer:
+class Forward:
     type_i: c_uint8
+
+@dataclass(kw_only=True)
+class ReLu(Forward):
+    type_i: c_uint8 = 0
+
+@dataclass(kw_only=True)
+class ArgMax(Forward):
+    type_i: c_uint8 = 1
+
+@dataclass
+class Layer(Forward):
     in_i: c_uint16
-    out_i: c_uint16 # length of matrix_a
-    matrix_a: list[Matrix]
+    out_i: c_uint16
+    matrix_a: list[Matrix] # length is out_i
 
-@dataclass
+@dataclass(kw_only=True)
 class FullConn(Layer):
-    type_i = 0
-    biases_i = c_uint16
-    biases_a = list[c_int8] # list[fxp-u8/7] (twos complement)
+    type_i: c_uint8 = 2
+    biases_i: c_uint16
+    biases_a: list[c_int8] # list[fxp-u8/7] (twos complement)
 
-@dataclass
+@dataclass(kw_only=True)
 class Conv(Layer):
-    type_i = 1
+    type_i: c_uint8 = 3
     kernel_i: c_uint8
-    padding_i: c_uint8
+    padding_i: c_uint8 = 0
+
+@dataclass(kw_only=True)
+class MaxPool(Forward):
+    type_i: c_uint8 = 4
+    kernel_i: c_uint8
+
+@dataclass(kw_only=True)
+class Flatten(Forward):
+    type_i: c_uint8 = 5
 
 @dataclass
 class Model:
-    layers_i: c_uint8
-    layers_a: list[Layer]
+    forwards_i: c_uint8
+    forwards_a: list[Forward]
 
 if __name__ == '__main__':
     use_cuda = torch.cuda.is_available()
@@ -61,6 +81,12 @@ if __name__ == '__main__':
     model = Net().to(device)
 
     requantize(model, safe_load('qmodel.safetensors'))
+    
+    forwards: list[Forward] = [
+        Conv(in_i=1, out_i=6, kernel_i=5, matrix_a=[]),
+    ]
+    
+    structure = Model(len(forwards), forwards)
     
     print(torch.flatten(model.conv1.weight._data, start_dim=1))
     print(torch.flatten(model.conv1.weight._scale))
