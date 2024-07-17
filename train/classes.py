d5d34e7d17229ac00dd1e850c79dc3319ecd0d4c
athalang <athalang@hotmail.com>
2024-07-17 12:16:22 +0100

    Add FC dataclass and refactor CSRMatrix

--- train/classes.py
+++ train/classes.py
@@ -4,8 +4,8 @@ from ctypes import c_int8, c_uint8, c_uint16
 @dataclass
 class Matrix:
     type_i: c_uint8
-    rows_i: c_uint16
-    cols_i: c_uint16
+    row_i: c_uint16
+    col_i: c_uint16
 
 @dataclass(kw_only=True)
 class ZeroMatrix(Matrix):
@@ -14,11 +14,10 @@ class ZeroMatrix(Matrix):
 @dataclass(kw_only=True)
 class CSRMatrix(Matrix):
     type_i: c_uint8 = 1
-    scale_f: c_uint16 # fxp-u16/15
     nnz_i: c_uint16 # number of non-zero elements
-    row_ptrs_a: list[c_uint16] # length is nnz_i
+    row_ptr_a: list[c_uint16] # length is nnz_i
     col_index_a: list[c_uint16] # length is nnz_i
-    values_a: list[c_int8] # length is nnz_i
+    value_a: list[c_int8] # length is nnz_i
 
 @dataclass
 class Forward:
@@ -40,9 +39,10 @@ class Layer(Forward):
 @dataclass(kw_only=True)
 class FullConn(Layer):
     type_i: c_uint8 = 2
-    biases_i: c_uint16
-    biases_a: list[c_uint16] # list[fxp-u16/15]
-    matrix_a: list[Matrix] # length is out_i
+    bias_i: c_uint16
+    bias_a: list[c_uint16] # list[fxp-u16/15]
+    scale_a: list[c_uint16] # list[fxp-u16/15]
+    matrix_o: Matrix
 
 @dataclass(kw_only=True)
 class Conv(Layer):
@@ -50,6 +50,7 @@ class Conv(Layer):
     kernel_i: c_uint8
     padding_i: c_uint8 = 0
     # list of out_i lists, with in_i matrices each
+    scale_aa: list[list[c_uint16]] # list[list[fxp-u16/15]]
     matrix_aa: list[list[Matrix]]
 
 @dataclass(kw_only=True)
--- train/serialise.py
+++ train/serialise.py
@@ -2,61 +2,82 @@ import torch
 
 from fxpmath import Fxp
 
-from optimum.quanto import safe_load, requantize, QConv2d
+from optimum.quanto import safe_load, requantize, QConv2d, QLinear
 
 from net import Net
 from classes import *
 
 def populateZeroMatrix(tensor: torch.Tensor) -> ZeroMatrix:
     matrix = ZeroMatrix(
-        rows_i=tensor.shape[0],
-        cols_i=tensor.shape[1],
+        row_i=tensor.shape[0],
+        col_i=tensor.shape[1],
     )
 
     return matrix
 
-def populateCSRMatrix(sparse: torch.Tensor, scale: float) -> CSRMatrix:
+def populateCSRMatrix(sparse: torch.Tensor) -> CSRMatrix:
     row_ptrs = [row.item() for row in sparse.crow_indices()[:-1]]
     col_indices = [col.item() for col in sparse.col_indices()]
     values = [val.item() for val in sparse.values()]
 
     matrix = CSRMatrix(
-        rows_i=sparse.shape[0],
-        cols_i=sparse.shape[1],
-        scale_f=Fxp(scale, dtype='fxp-u16/15'),
+        row_i=sparse.shape[0],
+        col_i=sparse.shape[1],
         # last element of crow_indices is nnz
         nnz_i=sparse.crow_indices()[-1].item(),
-        row_ptrs_a=row_ptrs,
+        row_ptr_a=row_ptrs,
         col_index_a=col_indices,
-        values_a=values,
+        value_a=values,
     )
 
     return matrix
 
 def populateConv(qconv: QConv2d) -> Conv:
-    nested: list[list[Matrix]] = []
+    nested_matrices: list[list[Matrix]] = []
+    nested_scales: list[list[c_uint16]] = []
 
     for out_i in range(0, qconv.out_channels):
         matrices: list[Matrix] = []
+        scales: list[c_uint16] = []
 
         for in_i in range(0, qconv.in_channels):
-            filter_scale = torch.flatten(qconv.weight._scale)[out_i].item()
             filter_weights = qconv.weight._data[out_i][in_i].to_sparse_csr()
+            filter_scale = torch.flatten(qconv.weight._scale)[out_i].item()
 
             if filter_scale == 0:
                 matrix = populateZeroMatrix(filter_weights)
             else:
-                matrix = populateCSRMatrix(filter_weights, filter_scale)
+                matrix = populateCSRMatrix(filter_weights)
 
             matrices.append(matrix)
+            scales.append(Fxp(filter_scale, dtype='fxp-u16/15'))
 
-        nested.append(matrices)
+        nested_matrices.append(matrices)
+        nested_scales.append(scales)
 
     return Conv(
         in_i=qconv.in_channels,
         out_i=qconv.out_channels,
         kernel_i=qconv.kernel_size[0],
-        matrix_aa=nested,
+        scale_aa=nested_scales,
+        matrix_aa=nested_matrices,
+    )
+
+def populateFC(qlin: QLinear) -> FullConn:
+    biases = [Fxp(bias.item(), dtype='fxp-u16/15') for bias in qlin.bias]
+
+    filter_scales = [Fxp(scale.item(), dtype='fxp-u16/15') for scale in torch.flatten(qlin.weight._scale)]
+    filter_weights = qlin.weight._data.to_sparse_csr()
+
+    matrix = populateCSRMatrix(filter_weights)
+
+    return FullConn(
+        in_i=qlin.in_features,
+        out_i=qlin.out_features,
+        bias_i=len(biases),
+        bias_a=biases,
+        scale_a=filter_scales,
+        matrix_o=matrix,
     )
 
 if __name__ == '__main__':
@@ -79,19 +100,14 @@ if __name__ == '__main__':
         ReLu(),
         MaxPool(kernel_i=2),
         Flatten(),
+        populateFC(model.fc1),
+        ReLu(),
+        populateFC(model.fc2),
+        ReLu(),
+        populateFC(model.fc3),
+        ArgMax(),
     ]
 
     structure = Model(len(forwards), forwards)
 
     print(forwards)
-
-    print(model.fc1.weight)
-    print(model.fc1.weight._data)
-    print(model.fc3.weight._data)
-    print(torch.flatten(model.fc3.weight._scale))
-    print(model.fc3.bias)
-    print(torch.flatten(model.fc1.weight._scale))
-    print(model.fc1.bias)
-    print(model.fc2.weight._data)
-    print(torch.flatten(model.fc2.weight._scale))
-    print(model.fc2.bias)
