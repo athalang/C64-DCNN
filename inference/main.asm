4161c3d30e6a88594d25d54b2139ec8418dd41cd
athalang <athalang@hotmail.com>
2024-07-23 23:20:23 +0100

    Add validation and forwards pointer setup

--- inference/main.asm
+++ inference/main.asm
@@ -1,19 +1,36 @@
 *=$0801
-
 !basic
-  ldx #$00
+
+  ; Jam if 0 layers in model
+  lda #0
+  cmp ModelBinary
+  beq FormatError
+
+  ; Store 2 * num of layers at $02
+  ldx #0
+  lda ModelBinary
+  asl
+  bcs FormatError ; Fail if overflow
+  sta $02
 
 ForwardsIter
+  ; Copy layer pointer to $03 and $04
+  inx ; Pointers start from 1 relative to ModelBinary
+  lda ModelBinary,x
+  sta $03
   inx
+  lda ModelBinary,x
+  sta $04
 
   ; Forward
 
-  cpx ModelBinary
+  cpx $02
   bne ForwardsIter
+
   rts
 
-SampleImage
-  !bin "../three.bin"
+FormatError jam
 
-ModelBinary
-  !bin "../model.bin"
+SampleImage !bin "../three.bin"
+
+ModelBinary !bin "../model.bin"
