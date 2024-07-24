4fbb4f43104fa04a8e2bff7a8d23ec100b00a692
athalang <athalang@hotmail.com>
2024-07-24 13:25:40 +0100

    Add multiply routine

--- inference/main.asm
+++ inference/main.asm
@@ -1,31 +1,43 @@
+!src "multiply.asm"
+
+ForwardIterMax = $08
+CurrentLayerLo = $09
+CurrentLayerHi = $0A
+
 *=$0801
 !basic
+  ; Switch out BASIC ROM
+  lda #%00110110
+  sta $01
+
+  ; Set up multiply routine
+  jsr init
 
   ; Jam if 0 layers in model
   lda #0
   cmp ModelBinary
   beq FormatError
 
-  ; Store 2 * num of layers at $02
+  ; Store 2 * num of layers at ForwardIterMax
   ldx #0
   lda ModelBinary
   asl
   bcs FormatError ; Fail if overflow
-  sta $02
+  sta ForwardIterMax
 
-ForwardsIter
+ForwardIter
   ; Copy layer pointer to $03 and $04
   inx ; Pointers start from 1 relative to ModelBinary
   lda ModelBinary,x
-  sta $03
+  sta CurrentLayerLo
   inx
   lda ModelBinary,x
-  sta $04
+  sta CurrentLayerHi
 
   ; Forward
 
-  cpx $02
-  bne ForwardsIter
+  cpx ForwardIterMax
+  bne ForwardIter
 
   rts
 
---
+++ inference/multiply.asm
@@ -0,0 +1,174 @@
+; from 6502.org, by Repose: http://forum.6502.org/viewtopic.php?p=106519#p106519
+; taken from https://github.com/TobyLobster/multiply_test
+
+; 16 bit x 16 bit unsigned multiply, 32 bit result
+; Average cycles: 187.07
+; 2170 bytes
+
+; How to use:
+; call jsr init, before first use
+; put numbers in (x0,x1) and (y0,y1) and result is (z3, A, Y, z0)
+
+; pointers to square tables
+p_sqr_lo1    = $8b   ; 2 bytes
+p_sqr_hi1    = $8d   ; 2 bytes
+p_neg_sqr_lo = $8f   ; 2 bytes
+p_neg_sqr_hi = $91   ; 2 bytes
+p_sqr_lo2    = $93   ; 2 bytes
+p_sqr_hi2    = $95   ; 2 bytes
+
+; the inputs and outputs
+x0  = p_sqr_lo1      ; multiplier, 2 bytes
+x1  = p_sqr_lo2
+y0  = $02            ; multiplicand, 2 bytes
+y1  = $03
+z0  = $04            ; product, 2 bytes + 2 registers
+                     ; z1  = $05 returned in Y reg
+                     ; z2  = $06 returned in A reg
+z3  = $07            ;
+
+* = $C700
+
+; Align tables to start of page
+; Note - the last byte of each table is never referenced, as a+b<=510
+sqrlo
+    !for i = 0 to 511
+        !byte <((i*i)/4)
+    !end
+sqrhi
+    !for i = 0 to 511
+        !byte >((i*i)/4)
+    !end
+
+negsqrlo
+    !for i = 0 to 511
+        !byte <(((255-i)*(255-i))/4)
+    !end
+
+negsqrhi
+    !for i = 0 to 511
+        !byte >(((255-i)*(255-i))/4)
+    !end
+
+; Diagram of the additions
+;                 y1    y0
+;              x  x1    x0
+;                 --------
+;              x0y0h x0y0l
+; +      x0y1h x0y1l
+; +      x1y0h x1y0l
+; +x1y1h x1y1l
+; ------------------------
+;     z3    z2    z1    z0
+
+umult16
+    ; set multiplier as x1
+    lda x1
+    sta p_sqr_hi1
+    eor #$ff
+    sta p_neg_sqr_lo
+    sta p_neg_sqr_hi
+
+    ; set multiplicand as y0
+    ldy y0
+
+    ; x1y0l =  low(x1*y0)
+    ; x1y0h = high(x1*y0)
+    sec
+    lda (p_sqr_lo2),y
+    sbc (p_neg_sqr_lo),y
+    sta x1y0l+1
+    lda (p_sqr_hi1), y
+    sbc (p_neg_sqr_hi),y
+    sta x1y0h+1
+
+    ; set multiplicand as y1
+    ldy y1
+
+    ; x1y1l =  low(x1*y1)
+    ; z3    = high(x1*y1)
+    lda (p_sqr_lo2),y
+    sbc (p_neg_sqr_lo),y
+    sta x1y1l+1
+    lda (p_sqr_hi1),y
+    sbc (p_neg_sqr_hi),y
+    sta z3
+
+    ; set multiplier as x0
+    lda x0
+    sta p_sqr_hi2
+    eor #$ff
+    sta p_neg_sqr_lo
+    sta p_neg_sqr_hi
+
+    ; x0y1l =  low(x0*y1)
+    ; X     = high(x0*y1)
+    lda (p_sqr_lo1),y
+    sbc (p_neg_sqr_lo),y
+    sta x0y1l+1
+    lda (p_sqr_hi2),y
+    sbc (p_neg_sqr_hi),y
+    tax
+
+    ; set multiplicand as y0
+    ldy y0
+
+    ; z0    =  low(x0*y0)
+    ; A     = high(x0*y0)
+    lda (p_sqr_lo1),y
+    sbc (p_neg_sqr_lo),y
+    sta z0
+    lda (p_sqr_hi2),y
+    sbc (p_neg_sqr_hi),y
+
+    clc
+do_adds
+    ; add the first two numbers of column 1
+x0y1l
+    adc #0      ; x0y0h + x0y1l
+    tay
+
+    ; continue to first two numbers of column 2
+    txa
+x1y0h
+    adc #0      ; x0y1h + x1y0h
+    tax         ; X=z2 so far
+    bcc +
+    inc z3      ; column 3
+    clc
+
+    ; add last number of column 1
++
+    tya
+x1y0l
+    adc #0      ; + x1y0l
+    tay         ; Y=z1
+
+    ; add last number of column 2
+    txa
+x1y1l
+    adc #0      ; + x1y1l
+    bcc fin     ; A=z2
+    inc z3      ; column 3
+fin
+    rts
+
+
+; Once only initialisation
+; this could set up the pointer values in a loop to save memory
+; it could also generate the square tables in code rather than load them
+init
+    lda #>sqrlo
+    sta p_sqr_lo2+1
+    sta p_sqr_lo1+1
+
+    lda #>sqrhi
+    sta p_sqr_hi1+1
+    sta p_sqr_hi2+1
+
+    lda #>negsqrlo
+    sta p_neg_sqr_lo+1
+
+    lda #>negsqrhi
+    sta p_neg_sqr_hi+1
+    rts
