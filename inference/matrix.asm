988accdbb394e1afb721750ea2282402060c2207
athalang <athalang@hotmail.com>
2024-08-05 23:41:54 +0100

    Add col indices and values

--- inference/matrix.asm
+++ inference/matrix.asm
@@ -4,8 +4,11 @@
 
 * = $0900
 
-.const curr_col		= $FB
-.const curr_pixel	= $FC // 2 bytes
+.const curr_col		= $2B
+.const curr_pixel	= $2C // 2 bytes
+.const curr_row_ptr	= $2E // 2 bytes
+.const curr_col_index	= $30 // 2 bytes
+.const curr_value	= $32 // 2 bytes
 
 matrix: {
 
@@ -15,10 +18,11 @@ overflow_err:	.byte JAM
 @sparse_input:	jsr mult_init
 
 		// Init zeropage counter
-		lda #dense_image
-		sta curr_pixel
-		lda #(dense_image >> 8) // Get first byte
-		sta curr_pixel+1
+		str_immediate_u16_u16(dense_image, curr_pixel)
+		str_immediate_u16_u16(INPUT_ROW_ARRAY, curr_row_ptr)
+		str_immediate_u16_u16(INPUT_COL_ARRAY, curr_col_index)
+		str_immediate_u16_u16(INPUT_VAL_ARRAY, curr_value)
+		str_immediate_u16_u16(0, curr_element)
 
 		ldx #0
 
@@ -27,9 +31,15 @@ overflow_err:	.byte JAM
 !loop:		sty curr_col
 		ldy #0		// Indirect zeropage, ignore Y
 		lda (curr_pixel),y
-		cmp #0
-		beq !+		// Branch when pixel is zero
+
+		beq !+		// Ignore when pixel is zero
+		sta (curr_value),y	// Current pixel in acc
+		add_immediate_u16(curr_value, 1, 0, curr_value)
 		add_immediate_u16(INPUT_NNZ, 1, 0, INPUT_NNZ)
+		add_immediate_u16(curr_element, 1, 0, curr_element)
+		lda curr_col
+		sta (curr_col_index),y
+		add_immediate_u16(curr_col_index, 1, 0, curr_col_index)
 
 !:		add_immediate_u16(curr_pixel, 1, 0, curr_pixel)
 		ldy curr_col
--- inference/utils.asm
+++ inference/utils.asm
@@ -2,6 +2,13 @@
 
 .const JAM	= $02	// Jam opcode
 
+.macro str_immediate_u16_u16 (imm, addr) {
+		lda #imm
+		sta addr
+		lda #(imm >> 8) // Get first byte
+		sta addr+1
+}
+
 .macro add_immediate_u16 (n, lo, hi, res) {
 		clc
 		lda n
@@ -16,26 +23,33 @@
 
 model_params:		.import binary "../model.bin"
 
-dense_image:		.import binary "../three.bin"
-
 // Dense input shape
 .const DENSE_ROWS	= 28
 .const DENSE_COLS	= 28
 .const DENSE_CELLS	= DENSE_ROWS * DENSE_COLS
 
-input_image:
-.byte %00000001			// Set matrix type to CSR
-.word DENSE_ROWS		// Set rows
-.word DENSE_COLS		// Set cols
-// Add enough space for the largest possible "sparse matrix"
-// i.e. when all cells are nonzero, will need 3 * nnz
-.fill DENSE_CELLS * 3 + 10, 0
+dense_image:		.import binary "../three.bin"
 
 // Addresses of input matrix properties
 .const INPUT_TYPE	= input_image		// 1 byte
-.const INPUT_ROWS	= input_image + 1	// 2 bytes
-.const INPUT_COLS	= input_image + 3	// 2 bytes
-.const INPUT_NNZ	= input_image + 5	// 2 bytes
-.const INPUT_ROWPTRS	= input_image + 7	// 2 bytes
-.const INPUT_COLINDEX	= input_image + 9	// 2 bytes
-.const INPUT_VALUES	= input_image + 11	// 2 bytes
+.const INPUT_ROWS	= input_image + 1	// 1 byte
+.const INPUT_COLS	= input_image + 2	// 1 byte
+.const INPUT_NNZ	= input_image + 3	// 2 bytes
+.const INPUT_ROW_ADDR	= input_image + 5	// 2 bytes
+.const INPUT_COL_ADDR	= input_image + 7	// 2 bytes
+.const INPUT_VAL_ADDR	= input_image + 9	// 2 bytes
+.const INPUT_ROW_ARRAY	= INPUT_VAL_ADDR + 2
+.const INPUT_COL_ARRAY	= INPUT_ROW_ARRAY + DENSE_ROWS * 2
+.const INPUT_VAL_ARRAY	= INPUT_COL_ARRAY + DENSE_CELLS
+
+input_image:
+// Set matrix type to CSR, row and col are 8 bit
+.byte %01100001
+.byte DENSE_ROWS	// Set rows
+.byte DENSE_COLS	// Set cols
+.word 0			// Clear nnz
+.word INPUT_ROW_ARRAY	// Set row ptr address
+.word INPUT_COL_ARRAY	// Set col index address
+.word INPUT_VAL_ARRAY	// Set value address
+// Add enough space for the largest possible "sparse matrix"
+.fill DENSE_CELLS * 3 + DENSE_ROWS * 2, 0
