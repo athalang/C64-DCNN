d7e5c818d609a4f4e0ec172f1d39eb8398ef68ef
athalang <athalang@hotmail.com>
2024-08-09 23:20:14 +0100

    Add free list consts

--- inference/mem.asm
+++ inference/mem.asm
@@ -1,35 +1,40 @@
 #importonce
 #import "utils.asm"
 
+// Blocks 8 byte aligned
+// Size + 2 unused bits + Free boolean = 2 bytes
+// Payload, then size + options repeated
+
+.const BLOCK_ALIGNMENT	= 8
+.const BLOCKS		= $1000		// Must be divisble by 2
+
+// Free list
+.const FREE_START	= $4000		// Needs to be aligned
+// Worst case, when every other block is free
+.const FREE_MAX		= BLOCKS / 2
+
+.const HEAP_START	= FREE_START + FREE_MAX
+.const HEAP_SIZE	= BLOCKS * BLOCK_ALIGNMENT
+// Plus 1 after the end of the heap, i.e. first byte not in heap
+.const HEAP_END_1P	= HEAP_START + HEAP_SIZE
+
+// Mask out unused bits and set free
+.const HEAP_OPTIONS	= (HEAP_SIZE & %1111111111111000) | 1
+
 * = $0901
 
 memory: {
 
-// Blocks 8 byte aligned
-// Size - 2 bytes
-// Least three bits used for options
-// When free block,
-// Previous free block - 2 bytes
-// Next free block - 2 bytes
-// Payload, then size + options repeated
-
-.const HEAP_START	= $4000
-.const HEAP_END		= $D000
-.const HEAP_SIZE	= HEAP_END - HEAP_START
-// Mask out unused bits and set free
-.const HEAP_HEADER	= (HEAP_SIZE & %1111111111111000) | 1
-
 @heap_init:	lda #%00110110 // Hide BASIC ROM
 		sta 1
 
-		str_immediate_u16_u16(HEAP_HEADER, HEAP_START)
-
-		// Clear next & prev pointers
-		str_immediate_u16_u16(0, HEAP_START+2)
-		str_immediate_u16_u16(0, HEAP_START+4)
-
-		str_immediate_u16_u16(HEAP_HEADER, HEAP_END-2)
+		str_immediate_u16_u16(HEAP_OPTIONS, HEAP_START)
+		str_immediate_u16_u16(HEAP_OPTIONS, HEAP_END_1P-2)
 
 		.byte JAM
 
+		rts
+
 } // End of scope memory
+
+* = $4000
